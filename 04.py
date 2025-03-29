7320600d78da6bb3c4aa0b2d1441a0df91508fc0
eunming90 <beaeunmi90@gmail.com>
2025-03-29 22:28:13 +0900

    Delete 04.py

--- 04.py
+++
@@ -1,7 +0,0 @@
-for i in range (11):
-    import math
-    radians = (math.pi / 180 ) * i 
-    sin_value = math.sin(radians)
-    cos_value = math.cos(radians)
-    tan_value = math.tan(radians)
-    print(f"{i}도 : {radians:.4f},sin {i} : {sin_value:.4f},cos {i} : {cos_value:.4f}, tan {i} :{tan_value:.4f}")
